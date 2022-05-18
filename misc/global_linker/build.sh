14c2bb2fba25f1199bc05dc19f0afb87e93a207b
David Oh <somyungoh@gmail.com>
2022-05-18 17:19:32 +0200

    Change link order of static and dyamic, now it produces what we want (SEGV)

--- misc/global_linker/build.sh
+++ misc/global_linker/build.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+echo "compiling static.cpp"
 # Compile static translation unit
 clang++ -std=c++14 -c -fpic static.cpp
 # Produce a static library
 ar rcs libstatic.a static.o
 
+echo "compiling dynamic.cpp"
 # Compile dynamic translation unit
 clang++ -std=c++14 -c -fpic dynamic.cpp
 # Produce dynamic library, linking against static
 clang++ -shared -o libdynamic.so dynamic.o -L./ -lstatic
 
+echo "compiling main.cpp"
 # Compile main execuatble translation unit
 clang++ -std=c++14 -c main.cpp
 # Link executable against static and shared libraries
-clang++ -L./ -Wl main.o -lstatic -ldynamic
+clang++ -L./ -Wl main.o -ldynamic -lstatic
